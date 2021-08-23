660da45c1636f280be84b3d8ffa82827f8938e4b
magefra9 <magefra9@hotmail.com>
2021-08-23 18:35:33 -0500

    Agregando proyectos iniciales

---
+++ Vehiculos/Vehiculos.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vehiculos.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Vehiculos/Vehiculos.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Vehiculos.Common
+{
+    public class Class1
+    {
+    }
+}
